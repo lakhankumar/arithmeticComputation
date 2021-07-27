50c528916ffe600b73922d27cbcc9750d437d815
lakhankumar <lakhankumar997@gmail.com>
2021-07-27 19:27:00 +0530

    Added: arithmetic dictionary

--- arithmeticComputation.sh
+++ arithmeticComputation.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+declare -A computeDictionary
+
 read -p "Enter a value: " a
 read -p "Enter b value: " b
 read -p "Enter c value: " c
@@ -8,6 +10,24 @@ echo "a= $a"
 echo "b= $b"
 echo "c= $c"
 
-temp=$(($a%$b))
-answer=$(($temp+$c))
-echo "answer: $answer"
+temp1=$(($a+$b))
+compute1=$(($temp1*$c))
+echo "answer: $compute1"
+
+temp2=$(($a*$b))
+compute2=$(($temp2+$c))
+echo "answer: $compute2"
+
+temp3=$(($c+$a))
+compute3=$(($temp3/$b))
+echo "answer: $compute3"
+
+temp4=$(($a%$b))
+compute4=$(($temp4+$c))
+echo "answer: $compute4"
+
+computeDictionary[eq1]=[$compute1]
+computeDictionary[eq2]=[$compute2]
+computeDictionary[eq3]=[$compute3]
+computeDictionary[eq4]=[$compute4]
+echo "${computeDictionary[*]}"
